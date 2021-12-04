86748c1675d8cd205c4a546bfe809734f9ef11e1
Pieter E Smit <github@vigor.nz>
2021-12-05 12:00:03 +1300

    move userdata setup into git/scripts/cloud-config-initial-setup.sh

---
+++ scripts/cloud-config-initial-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Called from initial cloud-config when ec2 instance created.
+
+cp /opt/gitrepo/scripts/docker.nginx.service /etc/systemd/system/docker.nginx.service
+systemctl daemon-reload
+
+systemctl enable docker.service
+systemctl,start --no-block, docker.service
+
+systemctl enable docker.nginx.service
+systemctl start --no-block docker.nginx.service
+
+
+echo "*/5 * * * * /opt/gitrepo/scripts/cron.sh 2>&1 | /dev/null" > /etc/cron.d/gitrepo
+
+# The END.
