7f3ae759e37f3bd1b3efbefecd1d6ea5dd3b92bd
Pieter E Smit <github@vigor.nz>
2021-12-06 08:32:00 +1300

    update health-status.py scheduled coroutine logging working

--- scripts/health-status.py
+++ scripts/health-status.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
+import logging
+from fastapi.logger import logger as fastapi_logger
 import asyncio
 import aiohttp
 import re
 import json
-#import logging
 import datetime
 import pytz
 #from yarl import cache_configure
-#logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("repeat")
+# # Console handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
 app = FastAPI()
+# fastapi_logger.setLevel(logging.DEBUG)  # :( ignored still only warn level logs
+# fastapi_logger.debug("Start debug - logger.")
+# fastapi_logger.info("Start info - logger.")
+# fastapi_logger.warn("Start warn - logger.")
  
 global config
 config = {
@@ -32,41 +42,52 @@ async def count():
 
 @app.on_event("startup")
 #@repeat_every(seconds=1, logger=logger, wait_first=True)
-@repeat_every(seconds=10, wait_first=True)
-async def run_10s_schedule(config=config):
+@repeat_every(seconds=10, wait_first=False, logger=logging.getLogger("repeat"))
+async def run_10s_schedule():
+    logger=logging.getLogger("repeat")
+    logger.setLevel(logging.INFO)
     global counter
+    global config
     counter += 1
-    print(f"START run_10s_schedule!!! #{counter}")
+    logger.debug(  f"START log-debug run_10s_schedule!!! #{counter}")
     loop = asyncio.get_running_loop()
-    task_docker = loop.create_task(getDockerStatus(name="nginx"))
-    task_nginx  = loop.create_task(getNginxStatus(status_url=config["status_url"]))
-    #
+    task_docker = loop.create_task(getDockerStatus(name="nginx", logger=logger))
+    task_nginx  = loop.create_task(getNginxStatus(status_url=config["status_url"], logger=logger))
+    logger.debug( "started status retrieval coroutines")
     utc_now = pytz.utc.localize(datetime.datetime.utcnow())
     local_now = utc_now.astimezone(pytz.timezone("Pacific/Auckland"))
     status_docker = await task_docker
     status_nginx  = await task_nginx
+    logger.debug( "retrieved status results")
     status = {"time": local_now.isoformat(), "docker" : status_docker, "nginx": status_nginx}
     with open(config["log_file"], "a") as file_object:
         file_object.write(json.dumps(status))
         file_object.write('\n')
-    print(f"END run_10s_schedule!!! #{counter} ##{status}")
+    logger.debug(f"END run_10s_schedule!!! #{counter} ##{status}")
 
 
-async def getDockerStatus(name: str):
+async def getDockerStatus(name: str, logger):
     # docker stats nginx   --no-stream   --format "{{ json . }}"
-    procdocker,exitcode = await run(f"docker stats --no-stream {name} --format '{{{{ json . }}}}'")
-    docker_info = { "exitcode": exitcode }
+    procdocker,exitcode = await run(f"docker stats --no-stream {name} --format '{{{{ json . }}}}'", logger)
+    docker_info = { "exitcode": exitcode, "Name": name }
+    logger.debug(f"getDockerStatus ... {exitcode}")
     if exitcode == 0:
         docker_info.update(json.loads(procdocker))
     return docker_info
 
 
-async def getNginxStatus(status_url: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(status_url) as resp:
-            nginx_text = await resp.text(encoding='ascii')
-        resp_dict = {"status": resp.status, "url": status_url}
-        if resp.status == 200:
+async def getNginxStatus(status_url: str, logger):
+    async with aiohttp.ClientSession(conn_timeout=2) as session:
+        try:
+            async with session.get(status_url) as resp:
+                nginx_text = await resp.text(encoding='ascii')
+        except aiohttp.client_exceptions.ServerTimeoutError:
+            response = "timeout"
+        else:
+            response = resp.status
+        logger.debug("getNginxStatus ...")
+        resp_dict = {"status": response, "url": status_url}
+        if response == 200:
             nginx_dict = await parse_nginx_status(nginx_text)
             resp_dict.update(nginx_dict)
             return resp_dict
@@ -94,7 +115,7 @@ async def parse_nginx_status(status_txt: str):
         return f"Re-Error: {status_txt}"
 
 
-async def run(cmd):
+async def run(cmd,logger):
     proc = await asyncio.create_subprocess_shell(
         cmd,
         stdout=asyncio.subprocess.PIPE,
@@ -105,7 +126,7 @@ async def run(cmd):
     #if stdout:
     #    print(f'[stdout]\n{stdout.decode()}')
     if stderr:
-        print(f'[stderr]\n{stderr.decode()}')
+        logger.error(f'[stderr]\n{stderr.decode()}')
     return stdout.decode(), proc.returncode
 
 if __name__ == "__main__":
