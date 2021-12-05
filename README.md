2b67067ef386db7e85fa21133530f448bdb8be3b
Pieter E Smit <github@vigor.nz>
2021-12-05 23:44:22 +1300

    update health-status.py retrieve nginx container info and write to logfile.

--- README.md
+++ README.md
@@ -22,11 +22,14 @@ level.
 7. Deploy and start a NGINX docker container in the EC2 instance.
 8. Deploy a script (or multiple scripts) on the EC2 instance to complete the following subtasks:
    a. Log the health status and resource usage of the NGINX container every 10 seconds into a log file.
+      Done- /var/log/resource.log
    b. Write a REST API using any choice of programming language which is you are familiar
-with and read from the above log file able to a basic search. (Provide us and
-example use of your API using curl or any REST client)
+      with and read from the above log file able to a basic search. (Provide us and
+      example use of your API using curl or any REST client)
+
 9. A README.md describing what you've done as well as steps explaining how to run the
 infrastructure automation and execute the script(s).
+
 10. Describe any risks associated with your application/deployment.
   - Risks:
       1. dos - single vm, no scaling or CDN
@@ -44,8 +47,8 @@ infrastructure automation and execute the script(s).
 
 Bonus Points
 1. Show the result of the resource.log on a webpage served from the NGINX server
-if you have any questions about the assignment feel free to reach out to us.
-
+   if you have any questions about the assignment feel free to reach out to us.
+   Done /resource.log
 # Prerequisites
   1. install terraform commandline util >=v1.0.11
      * https://www.terraform.io/downloads.html
--- scripts/health-status.py
+++ scripts/health-status.py
@@ -3,10 +3,22 @@ from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
 import asyncio
 import aiohttp
-import logging
-logging.basicConfig(level=logging.DEBUG)
+import re
+import json
+#import logging
+import datetime
+import pytz
+#from yarl import cache_configure
+#logging.basicConfig(level=logging.INFO)
 app = FastAPI()
  
+global config
+config = {
+    #"log_file": "/opt/gitrepo/html/resource.log",
+    #status_url: "http://127.0.0.1:81/nginx-status",
+    "log_file":   "/var/log/monitor-nginx.log",
+    "status_url": "http://52.64.137.163:81/nginx_status",
+}
 global counter
 counter = 1
 
@@ -21,45 +33,66 @@ async def count():
 @app.on_event("startup")
 #@repeat_every(seconds=1, logger=logger, wait_first=True)
 @repeat_every(seconds=10, wait_first=True)
-async def run_10s_schedule():
+async def run_10s_schedule(config=config):
     global counter
     counter += 1
+    print(f"START run_10s_schedule!!! #{counter}")
     loop = asyncio.get_running_loop()
     task_docker = loop.create_task(getDockerStatus(name="nginx"))
-    task_nginx  = loop.create_task(getNginxStatus(url="http://127.0.0.1:81/nginx-status"))
-
+    task_nginx  = loop.create_task(getNginxStatus(status_url=config["status_url"]))
+    #
+    utc_now = pytz.utc.localize(datetime.datetime.utcnow())
+    local_now = utc_now.astimezone(pytz.timezone("Pacific/Auckland"))
     status_docker = await task_docker
-    print(f"END run_10s_schedule!!! #{counter} #{status_docker}")
     status_nginx  = await task_nginx
-
-    print(f"END run_10s_schedule!!! #{counter} #{status_docker} #{status_nginx}")
+    status = {"time": local_now.isoformat(), "docker" : status_docker, "nginx": status_nginx}
+    with open(config["log_file"], "a") as file_object:
+        file_object.write(json.dumps(status))
+        file_object.write('\n')
+    print(f"END run_10s_schedule!!! #{counter} ##{status}")
 
 
 async def getDockerStatus(name: str):
     # docker stats nginx   --no-stream   --format "{{ json . }}"
-    print("DockerOK-start")
-    procdocker,returncode = await run(f"docker stats --no-stream {name} --format '{{{{ json . }}}}'")
-    print("DockerOK-done")
-    if returncode == 0:
-        return f"DockerOK:{procdocker}"
-    else:
-        return "DockerErr:"
+    procdocker,exitcode = await run(f"docker stats --no-stream {name} --format '{{{{ json . }}}}'")
+    docker_info = { "exitcode": exitcode }
+    if exitcode == 0:
+        docker_info.update(json.loads(procdocker))
+    return docker_info
 
 
-async def getNginxStatus(url: str):
-    print("NginxOK-start")
+async def getNginxStatus(status_url: str):
     async with aiohttp.ClientSession() as session:
-        status_url = 'http://127.0.0.1:81/nginx_status'
         async with session.get(status_url) as resp:
-            nginx_status = await resp.json()
-            print(nginx_status)
-    status = '''Active connections: 1 
-server accepts handled requests
- 692 692 691 
-Reading: 0 Writing: 1 Waiting: 0 
-'''
-    print("NginxOK-end")
-    return "Nginx ok"
+            nginx_text = await resp.text(encoding='ascii')
+        resp_dict = {"status": resp.status, "url": status_url}
+        if resp.status == 200:
+            nginx_dict = await parse_nginx_status(nginx_text)
+            resp_dict.update(nginx_dict)
+            return resp_dict
+        else:
+            return resp_dict
+
+
+async def parse_nginx_status(status_txt: str):
+    ''' example status_txt
+    Active connections: 1 
+    server accepts handled requests
+    692 692 691 
+    Reading: 0 Writing: 1 Waiting: 0 
+    '''
+    match = re.match(r'Active connections: (?P<active>\d+)\s*\n'+
+                     r'server accepts handled requests\s*\n'+
+                     r'\s*(?P<accepts>\d+)\s+(?P<handled>\d+)\s+(?P<requests>\d+)\s*\n'+
+                     r'\s*Reading:\s+(?P<reading>\d+)\s+Writing:\s+(?P<writing>\d+)\s+Waiting:\s+(?P<waiting>\d+)\s*$'
+                     ,status_txt
+                     ,re.MULTILINE|re.DOTALL
+                    )
+    if match:
+        return match.groupdict()
+    else:
+        return f"Re-Error: {status_txt}"
+
 
 async def run(cmd):
     proc = await asyncio.create_subprocess_shell(
