937b838f5c43e64f8903e8a3e3aeb93e5160ffdc
Pieter E Smit <github@vigor.nz>
2021-12-06 21:45:52 +1300

    update docker.nginx.service remove comment, update target_group port to 8080

--- terraform/asg+lb.tf
+++ terraform/asg+lb.tf
@@ -78,7 +78,7 @@ resource "aws_lb_listener" "front_end" {
 }
 resource "aws_lb_target_group" "front_end" {
   name     = "${var.prefix}-target-group"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
 }
