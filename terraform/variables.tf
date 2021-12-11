10bfe7a436a68cc749ca8739d9eea8e32719de5c
Pieter E Smit <github@vigor.nz>
2021-12-11 13:46:20 +1300

    update ec2-userdata.yaml add iptables portlimit to docker-user

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,3 @@
-variable "pub_key_path" {
-  type        = string
-  description = "path to public key to use for new instance setup."
-}
-variable "pub_key_name" {
-  type        = string
-  description = "name of key in aws."
-}
-
 variable "gitrepo" {
   type        = string
   description = "Git repo to be cloned into /opt/gitrepo , contains ./scripts and ./html"
